693c41af0cebbd411b07d1f350ee40ac6611b919
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-02-15 01:46:20 +0700

    update sign in

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 double defaultRadius = 12.0;
-double defaultMargin = 30.0;
+double defaultMargin = 36.0;
 
 Color backgroundColor = const Color(0xff282727);
 Color primaryColor = const Color(0xffFFFFFF);
 Color secondaryColor = const Color(0xff7B6AE1);
+Color greyColor = const Color(0xffBDBDBD);
 
 TextStyle primaryColorText = GoogleFonts.poppins(color: primaryColor);
 TextStyle secondaryColorText = GoogleFonts.poppins(color: secondaryColor);
+TextStyle greyColorText = GoogleFonts.poppins(color: greyColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
--- lib/sign_in_page.dart
+++ lib/sign_in_page.dart
@@ -6,9 +6,13 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController emailController =
+        TextEditingController(text: '');
+    final TextEditingController passwordController =
+        TextEditingController(text: '');
     Widget header() {
       return Container(
-        padding: const EdgeInsets.only(top: 110, left: 36),
+        padding: EdgeInsets.only(top: 110, left: defaultMargin),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -44,13 +48,117 @@ class SignInPage extends StatelessWidget {
       );
     }
 
+    Widget inputEmail() {
+      return Container(
+          margin: const EdgeInsets.only(top: 3),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: TextFormField(
+            controller: emailController,
+            style: primaryColorText.copyWith(fontSize: 13),
+            cursorColor: primaryColor,
+            decoration: InputDecoration(
+                prefixIcon: Icon(
+                  Icons.email_outlined,
+                  color: primaryColor,
+                  size: 21,
+                ),
+                hintText: "Enter your email address",
+                hintStyle: primaryColorText.copyWith(fontSize: 16),
+                focusedBorder: UnderlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: primaryColor, width: 5)),
+                enabledBorder: UnderlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(
+                      color: primaryColor,
+                      width: 3,
+                      style: BorderStyle.solid,
+                    ))),
+          ));
+    }
+
+    Widget inputPassword() {
+      return Container(
+          margin: const EdgeInsets.only(top: 3),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: TextFormField(
+            controller: passwordController,
+            obscureText: true,
+            style: primaryColorText.copyWith(fontSize: 13),
+            cursorColor: primaryColor,
+            decoration: InputDecoration(
+                suffixIcon: Icon(
+                  Icons.visibility_off,
+                  color: primaryColor,
+                ),
+                prefixIcon: Icon(
+                  Icons.lock_outline,
+                  color: primaryColor,
+                  size: 21,
+                ),
+                hintText: "Enter your email address",
+                hintStyle: primaryColorText.copyWith(fontSize: 16),
+                focusedBorder: UnderlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(color: primaryColor, width: 5)),
+                enabledBorder: UnderlineInputBorder(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    borderSide: BorderSide(
+                      color: primaryColor,
+                      width: 3,
+                      style: BorderStyle.solid,
+                    ))),
+          ));
+    }
+
+    Widget forgotPassword() {
+      return Container(
+        padding: const EdgeInsets.only(right: 31),
+        margin: const EdgeInsets.only(top: 8),
+        child: GestureDetector(
+            onTap: () {},
+            child: Text(
+              "Forgot Password?",
+              style: greyColorText,
+            )),
+      );
+    }
+
+    Widget content() {
+      return ListView(
+        children: [
+          header(),
+          Container(
+              padding: EdgeInsets.only(left: defaultMargin),
+              margin: const EdgeInsets.only(top: 52),
+              child: Text(
+                "Email",
+                style:
+                    primaryColorText.copyWith(fontSize: 13, fontWeight: medium),
+              )),
+          inputEmail(),
+          Container(
+              padding: EdgeInsets.only(left: defaultMargin),
+              margin: const EdgeInsets.only(top: 49),
+              child: Text(
+                "Password",
+                style:
+                    primaryColorText.copyWith(fontSize: 13, fontWeight: medium),
+              )),
+          inputPassword(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              forgotPassword(),
+            ],
+          )
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: SafeArea(
-        child: ListView(
-          children: [header()],
-        ),
-      ),
+      body: SafeArea(child: content()),
     );
   }
 }
