ac36f2a0fca20f68dfabe35d5415e6f2a4405855
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-02-20 14:34:10 +0700

    added audio player page

---
+++ lib/pages/pages_user/audio_player_page.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/shared/theme.dart';
+
+class AudioPlayerPage extends StatelessWidget {
+  const AudioPlayerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            child: Icon(
+              Icons.keyboard_arrow_down_rounded,
+              color: primaryColor,
+            ),
+          ),
+          Text(
+            "playing from playlist",
+            style: primaryColorText.copyWith(fontSize: 12),
+          ),
+          GestureDetector(
+            child: Icon(
+              Icons.more_vert,
+              color: primaryColor,
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget music() {
+      return Container(
+        margin: const EdgeInsets.only(top: 16),
+        child: Column(
+          children: [
+            Text(
+              "Favourite of Maliq & D’Essentials",
+              style: primaryColorText.copyWith(fontSize: 16, fontWeight: bold),
+            ),
+            const SizedBox(
+              height: 55,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(32),
+              child: Image.asset(
+                "assets/audio_page.png",
+                height: 280,
+                width: 280,
+              ),
+            ),
+            const SizedBox(
+              height: 64,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Terdiam",
+                  style:
+                      primaryColorText.copyWith(fontSize: 16, fontWeight: bold),
+                ),
+                Icon(
+                  Icons.favorite,
+                  color: primaryColor,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "2:10",
+                  style: primaryColorText.copyWith(fontSize: 12),
+                ),
+                Text("4:58", style: primaryColorText.copyWith(fontSize: 12))
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 5, bottom: 50),
+              width: double.infinity,
+              height: 8,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: LinearProgressIndicator(
+                  backgroundColor: backgroundProgressIndicatorColor,
+                  value: 2.1 / 4.58,
+                  color: progressIndicatorColor,
+                  minHeight: 8,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  Icons.shuffle,
+                  color: primaryColor,
+                  size: 28,
+                ),
+                Icon(
+                  Icons.fast_rewind_outlined,
+                  color: primaryColor,
+                  size: 34,
+                ),
+                Icon(
+                  Icons.play_circle_fill_rounded,
+                  color: primaryColor,
+                  size: 34,
+                ),
+                Icon(
+                  Icons.fast_forward_outlined,
+                  color: primaryColor,
+                  size: 34,
+                ),
+                Icon(
+                  Icons.replay,
+                  color: primaryColor,
+                  size: 28,
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(horizontal: defaultMargin, vertical: 60),
+        children: [header(), music()],
+      );
+    }
+
+    return Scaffold(
+        backgroundColor: backgroundColor, body: SafeArea(child: content()));
+  }
+}
--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -8,6 +8,9 @@ Color backgroundColor = const Color(0xff282727);
 Color primaryColor = const Color(0xffFFFFFF);
 Color secondaryColor = const Color(0xff7B6AE1);
 Color greyColor = const Color(0xffBDBDBD);
+Color progressIndicatorColor = const Color(0xff6D6D6D);
+Color backgroundProgressIndicatorColor =
+    const Color.fromARGB(30, 196, 196, 196);
 
 TextStyle primaryColorText = GoogleFonts.poppins(color: primaryColor);
 TextStyle secondaryColorText = GoogleFonts.poppins(color: secondaryColor);
