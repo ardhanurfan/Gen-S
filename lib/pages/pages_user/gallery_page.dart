e753de888925fd93f7309a5b148253bf3a6e0e22
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-03-01 15:56:52 +0700

    added empty gallery

--- lib/pages/pages_user/gallery_page.dart
+++ lib/pages/pages_user/gallery_page.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:music_player/shared/theme.dart';
+import 'package:music_player/widgets/custom_button.dart';
 
 class GalleryPage extends StatelessWidget {
   const GalleryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.all(defaultMargin),
+        children: [
+          Text(
+            "Album Name",
+            textAlign: TextAlign.end,
+            style: primaryColorText.copyWith(fontSize: 20, fontWeight: bold),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 100, bottom: 69),
+            child: Image.asset(
+              "assets/empty_gallery.png",
+              width: 345,
+              height: 184,
+            ),
+          ),
+          Text(
+            "Nothing here",
+            textAlign: TextAlign.center,
+            style: primaryColorText.copyWith(fontSize: 20, fontWeight: bold),
+          ),
+          const SizedBox(
+            height: 22,
+          ),
+          CustomButton(
+              radiusButton: 32,
+              buttonColor: secondaryColor,
+              buttonText: "Add Picture",
+              onPressed: () {},
+              heightButton: 53)
+        ],
+      );
+    }
+
     return Scaffold(
-      backgroundColor: backgroundColor,
-      body: const Center(
-        child: Text('Gallery Page'),
-      ),
-    );
+        backgroundColor: backgroundColor, body: SafeArea(child: content()));
   }
 }
