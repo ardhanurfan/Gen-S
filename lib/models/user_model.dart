eaea1e667f85ee9f9314cfde48e992e4b07b31c2
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-03-16 21:35:52 +0700

    added settings page

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,12 +3,14 @@ import 'package:equatable/equatable.dart';
 class UserModel extends Equatable {
   final int id;
   final String email;
+  final String photoUrl;
   final String username;
   final String role;
   final String token;
 
   const UserModel({
     required this.id,
+    required this.photoUrl,
     required this.email,
     required this.username,
     required this.role,
@@ -18,6 +20,7 @@ class UserModel extends Equatable {
   factory UserModel.fromJson(Map<String, dynamic> json, String token) {
     return UserModel(
       id: json['id'],
+      photoUrl: json['profile_photo_url'],
       email: json['email'],
       username: json['username'],
       role: json['role'],
--- lib/widgets/setting_button.dart
+++ lib/widgets/setting_button.dart
@@ -1,84 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:music_player/providers/user_provider.dart';
-import 'package:music_player/shared/theme.dart';
-import 'package:provider/provider.dart';
+import 'package:music_player/pages/settings_page.dart';
 
-class SettingButton extends StatefulWidget {
+class SettingButton extends StatelessWidget {
   const SettingButton({
     Key? key,
   }) : super(key: key);
 
   @override
-  State<SettingButton> createState() => _SettingButtonState();
-}
-
-bool isLoading = false;
-
-class _SettingButtonState extends State<SettingButton> {
-  @override
   Widget build(BuildContext context) {
-    UserProvider userProvider = Provider.of<UserProvider>(context);
-
-    handleLogout() async {
-      setState(() {
-        isLoading = true;
-      });
-
-      if (await userProvider.logout(token: userProvider.user.token)) {
-        Navigator.pushNamedAndRemoveUntil(
-            context, '/sign-in', (route) => false);
-      } else {
-        ScaffoldMessenger.of(context).removeCurrentSnackBar();
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            backgroundColor: alertColor,
-            content: Text(
-              userProvider.errorMessage,
-              textAlign: TextAlign.center,
-            ),
-          ),
-        );
-      }
-
-      setState(() {
-        isLoading = false;
-      });
-    }
-
-    return PopupMenuButton(
-      color: primaryColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(defaultRadius),
-      ),
-      elevation: 4,
-      onSelected: (value) {
-        if (value == 0) {
-          handleLogout();
-        }
+    return GestureDetector(
+      child: const Icon(Icons.settings_outlined),
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const SettingsPage()));
       },
-      itemBuilder: (context) => [
-        PopupMenuItem(
-          value: 0,
-          child: Text(
-            'Logout',
-            style: TextStyle(color: alertColor),
-          ),
-        ),
-      ],
-      child: isLoading
-          ? Container(
-              padding: const EdgeInsets.all(4),
-              height: 32,
-              width: 32,
-              child: CircularProgressIndicator(
-                color: primaryColor,
-              ),
-            )
-          : Icon(
-              Icons.settings_outlined,
-              color: primaryColor,
-              size: 32,
-            ),
     );
   }
 }
