2316b59b6623c311be54beb79c6434863bad7926
Ibrahim Imam <ibrhmmm552@gmail.com>
2023-03-23 16:06:02 +0700

    update ads

--- lib/pages_admin/ads_detail_page.dart
+++ lib/pages_admin/ads_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:music_player/widgets/custom_button.dart';
 
 import '../shared/theme.dart';
 
@@ -13,10 +14,9 @@ class AdsDetailPage extends StatefulWidget {
 class _AdsDetailPageState extends State<AdsDetailPage> {
   DateTime dateTime = DateTime.now();
   TextEditingController titleController = TextEditingController(text: "");
+  TextEditingController frequencyController = TextEditingController(text: "");
   @override
   Widget build(BuildContext context) {
-    final hours = dateTime.hour.toString().padLeft(2, '0');
-    final minutes = dateTime.minute.toString().padLeft(2, '0');
     Widget header() {
       return Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -44,75 +44,107 @@ class _AdsDetailPageState extends State<AdsDetailPage> {
         padding: EdgeInsets.symmetric(
             horizontal: defaultMargin, vertical: defaultMargin),
         children: [
-          header(),
-          Padding(
-            padding:
-                const EdgeInsets.only(top: 60, left: 50, right: 50, bottom: 30),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(4),
-              child: Image.asset(
-                "assets/ex_gallery.png",
-                height: 220,
-                fit: BoxFit.fill,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              header(),
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 60, left: 50, right: 50, bottom: 30),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(4),
+                  child: Image.asset(
+                    "assets/ex_gallery.png",
+                    height: 220,
+                    fit: BoxFit.fill,
+                  ),
+                ),
               ),
-            ),
-          ),
-          Text(
-            "Title",
-            style: primaryAdminColorText.copyWith(
-                fontSize: 14, fontWeight: medium),
-          ),
-          const SizedBox(
-            height: 3,
-          ),
-          TextField(
-            controller: titleController,
-            style: primaryAdminColorText.copyWith(fontSize: 14),
-            cursorColor: primaryAdminColor,
-            decoration: InputDecoration(
-                hintStyle: primaryAdminColorText.copyWith(fontSize: 16),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: primaryAdminColor))),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          Text(
-            "Time",
-            style: primaryAdminColorText,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          GestureDetector(
-            onTap: pickDateTime,
-            child: Column(
-              children: [
-                Row(
+              Text(
+                "Title",
+                style: primaryAdminColorText.copyWith(
+                    fontSize: 14, fontWeight: medium),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              TextField(
+                controller: titleController,
+                style: primaryAdminColorText.copyWith(fontSize: 14),
+                cursorColor: primaryAdminColor,
+                decoration: InputDecoration(
+                    hintStyle: primaryAdminColorText.copyWith(fontSize: 16),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: primaryAdminColor))),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Time",
+                style: primaryAdminColorText,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onTap: pickDateTime,
+                child: Column(
                   children: [
-                    Icon(
-                      Icons.calendar_today_rounded,
-                      color: primaryAdminColor,
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Column(
+                    Row(
                       children: [
-                        Text(DateFormat('yyyy/MM/dd - hh:mm').format(dateTime),
-                            style: primaryAdminColorText.copyWith()),
+                        Icon(
+                          Icons.calendar_today_rounded,
+                          color: primaryAdminColor,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                                DateFormat('yyyy/MM/dd - hh:mm')
+                                    .format(dateTime),
+                                style: primaryAdminColorText.copyWith()),
+                          ],
+                        )
                       ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 8),
+                      width: double.infinity,
+                      height: 0.85,
+                      color: primaryAdminColor,
                     )
                   ],
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 8),
-                  width: double.infinity,
-                  height: 0.85,
-                  color: primaryAdminColor,
-                )
-              ],
-            ),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Frequency",
+                style: primaryAdminColorText,
+              ),
+              TextField(
+                controller: titleController,
+                style: primaryAdminColorText.copyWith(fontSize: 14),
+                cursorColor: primaryAdminColor,
+                decoration: InputDecoration(
+                    hintStyle: primaryAdminColorText.copyWith(fontSize: 16),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: primaryAdminColor))),
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              CustomButton(
+                  radiusButton: 32,
+                  buttonColor: secondaryColor,
+                  buttonText: "Save",
+                  onPressed: () {},
+                  heightButton: 53)
+            ],
           ),
         ],
       );
@@ -131,7 +163,7 @@ class _AdsDetailPageState extends State<AdsDetailPage> {
     if (time == null) return;
 
     final pickedDateTime =
-        DateTime(date.year, date.month, date.second, time.hour, time.minute);
+        DateTime(date.year, date.month, date.day, time.hour, time.minute);
 
     setState(() {
       dateTime = pickedDateTime;
@@ -141,8 +173,8 @@ class _AdsDetailPageState extends State<AdsDetailPage> {
   Future<DateTime?> pickDate() => showDatePicker(
       context: context,
       initialDate: dateTime,
-      firstDate: dateTime,
-      lastDate: DateTime(dateTime.year + 100));
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2500));
 
   Future<TimeOfDay?> pickTime() => showTimePicker(
       context: context,
